037447110603c6ddb64d98a7723b378f993660d8
frank7841 <franklinewax@gmail.com>
2020-07-05 11:39:16 +0300

    Finale commit

---
+++ src/main/java/App.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
+        get("/favorite_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
